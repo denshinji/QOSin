0597c2dd97aa464b960bab26fbddbfa9f22814c2
DENSHIN <denialfarizi.da@gmail.com>
2020-01-17 06:36:22 +0700

    Update UI

---
+++ app/src/main/java/com/mobile/qosin/Adapter/SliderAdapter.java
@@ -0,0 +1,4 @@
+package com.mobile.qosin.Adapter;
+
+public class SliderAdapter {
+}
---
+++ app/src/main/java/com/mobile/qosin/MenuLogin.java
@@ -0,0 +1,13 @@
+package com.mobile.qosin;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_login);
+    }
+}
