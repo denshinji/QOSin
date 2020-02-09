fb9e69efc113a71f143f5ad40bea70a4418a8c69
DENSHIN <denialfarizi.da@gmail.com>
2020-02-10 01:49:10 +0700

    After create custom bottom navigation

--- app/src/main/java/com/mobile/qosin/Activity/MainActivity.java
+++ app/src/main/java/com/mobile/qosin/Activity/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
@@ -19,14 +20,24 @@ import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.mobile.qosin.Adapter.CustomBottomBar;
+import com.mobile.qosin.Adapter.ItemAdapter;
 import com.mobile.qosin.Dashboard.Dashboard;
+import com.mobile.qosin.Model.CustomBottomItem;
 import com.mobile.qosin.R;
 import com.mobile.qosin.SessionManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ItemAdapter.ItemSelectorInterface {
     private boolean doubleBackToExitPressedOnce = false;
     private SessionManager sessionManager;
     private Button logout;
+    private CustomBottomBar customBottomBar;
+    public static final int HOME = 0;
+    public static final int FAVORITE = 1;
+    public static final int PROFILE = 2;
+
+
+    @SuppressLint("ResourceType")
 
 
     @Override
@@ -39,14 +50,26 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar_include);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(null);
-        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
-        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.frame_main, dashboard);
-        transaction.addToBackStack(null);
-        transaction.commit();
-    }
 
+        customBottomBar = new CustomBottomBar(this, findViewById(R.id.customBottomBar), MainActivity.this);
+        initItems();
+        customBottomBar.changeBackground(getString(R.color.colorItemDefaultBackground));
+        customBottomBar.setDefaultBackground(getString(R.color.colorItemDefaultBackground));
+        customBottomBar.setDefaultTint(getString(R.color.colorItemDefaultTint));
+        customBottomBar.changeDividerColor(getString(R.color.colorDivider));
+        customBottomBar.hideDivider();
+        customBottomBar.apply(HOME);
+    }
+    @SuppressLint("ResourceType")
+    private void initItems() {
+        CustomBottomItem home = new CustomBottomItem(HOME, R.drawable.icon_dashboard_home, getString(R.string.home_title), getString(R.color.colorItemBackground), getString(R.color.colorHijau));
+        CustomBottomItem favorite = new CustomBottomItem(FAVORITE, R.drawable.ic_favorite_black_24dp, getString(R.string.favorite_title), getString(R.color.colorItem1Background), getString(R.color.colorHijau));
+        CustomBottomItem profile = new CustomBottomItem(PROFILE, R.drawable.profile_icon, getString(R.string.profile_title), getString(R.color.colorItem2Background), getString(R.color.colorHijau));
+
+        customBottomBar.addItem(home);
+        customBottomBar.addItem(favorite);
+        customBottomBar.addItem(profile);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -133,4 +156,19 @@ public class MainActivity extends AppCompatActivity {
                 }
             };
 
+    @Override
+    public void itemSelect(int selectedID) {
+        Fragment selectedFragment = null;
+        switch (selectedID){
+            case HOME:
+                selectedFragment = new Dashboard();
+                break;
+            case FAVORITE:
+                selectedFragment = new Dashboard();
+                break;
+            case PROFILE:
+                 selectedFragment = new Account();
+                break;
+        }
+    }
 }
